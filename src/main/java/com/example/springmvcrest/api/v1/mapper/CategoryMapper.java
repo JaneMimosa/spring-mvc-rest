c9192faebd6ce6eb359197474599eca07d36f425
JaneMimosa <jane666bender@gmail.com>
2023-03-11 18:02:18 +0300

    Added mapstruct support plugin

--- src/main/java/com/example/springmvcrest/api/v1/mapper/CategoryMapper.java
+++ src/main/java/com/example/springmvcrest/api/v1/mapper/CategoryMapper.java
@@ -4,6 +4,7 @@ package com.example.springmvcrest.api.v1.mapper;
 import com.example.springmvcrest.api.v1.model.CategoryDTO;
 import com.example.springmvcrest.domain.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,6 +12,7 @@ public interface CategoryMapper {
 
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
 
+    @Mapping(source = "id", target = "id")
     CategoryDTO categoryToCategoryDTO(Category category);
 
 }
