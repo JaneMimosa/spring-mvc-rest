ae4006b079086048f5c02cf8c91c5e3b1c1481c5
JaneMimosa <jane666bender@gmail.com>
2023-03-14 18:41:29 +0300

    Added missing annotations and package scan

--- src/main/java/com/example/springmvcrest/SpringMvcRestApplication.java
+++ src/main/java/com/example/springmvcrest/SpringMvcRestApplication.java
@@ -2,8 +2,10 @@ package com.example.springmvcrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.springmvcrest.api.v1.mapper"})
 public class SpringMvcRestApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/springmvcrest/repositories/VendorRepository.java
+++ src/main/java/com/example/springmvcrest/repositories/VendorRepository.java
@@ -2,6 +2,8 @@ package com.example.springmvcrest.repositories;
 
 import com.example.springmvcrest.domain.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VendorRepository extends JpaRepository<Vendor, Long> {
 }
